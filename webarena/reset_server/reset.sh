d33d6baeec1b49422b68c7803bf1842b17e1cfbb
Nicholas Lee <dragon18456@hotmail.com>
2025-12-12 18:52:15 -0800

    Update reset.sh

--- webarena/reset_server/reset.sh
+++ webarena/reset_server/reset.sh
@@ -4,8 +4,8 @@
 set -e
 
 cd ..
-bash 02_docker_remove_containers.sh
-bash 03_docker_create_containers.sh
-bash 04_docker_start_containers.sh
-bash 05_docker_patch_containers.sh
+sudo bash 02_docker_remove_containers.sh
+sudo bash 03_docker_create_containers.sh
+sudo bash 04_docker_start_containers.sh
+sudo bash 05_docker_patch_containers.sh
 
